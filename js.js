0b5f3c2e68c1687a974b5455daaae18d371fb631
Ahmed Oudouh <oudouhahmed@gmail.com>
2021-08-15 21:10:43 +0100

    finished

--- js.js
+++ js.js
@@ -1,150 +1,114 @@
-let theInput = document.querySelector(".add-task input");
-let theAddButton = document.querySelector(".plus");
-let ulList = document.querySelector(".tasks-content")
-let inputadd = document.querySelector(".input-text");
-///////////////////////////////////////////// date ///////////////////////////////////////////////////////////
+let buttonSubmitElemnt = document.getElementById("button-submit")
+let inputElemnt = document.getElementById("input")
+let ulListElemnt = document.querySelector(".tasks-content")
+let searchElemnt = document.getElementById("search")
+    //
+
+//date
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 var today = new Date();
 var dd = String(today.getDate());
 var mm = String(months[today.getMonth()]);
 var yyyy = today.getFullYear();
 today = dd + 'th ' + mm + ', ' + yyyy;
-
+//
 let date = document.getElementById("date").innerHTML = today;
 
-///////////////////////////////////////////////////// disabled //////////////////////////////////////
-inputadd.addEventListener('keyup', function(e) {
-    if (e.target.value !== "") {
-        theAddButton.disabled = false;
-    }
-
-});
-//////////////////////////////////////////////////// Adding The Task ////////////////////////////////////////
-//
-let addContent = () => {
-    // createElement"li" and createclassname "task-box"
-    let itemAdd = document.createElement("li");
-    itemAdd.className = 'task-box';
-    ulList.prepend(itemAdd);
-    //createElement "input" and Attribute "radio"
-    let inputRadio = document.createElement("input");
-    inputRadio.setAttribute('type', 'radio')
-        // add input in  itemAdd last
-    inputRadio.className = "delete";
-    itemAdd.append(inputRadio);
-
-    let span = document.createElement('span')
-    span.className = "item";
-    itemAdd.append(span);
-};
-
-theAddButton.onclick = function() {
-    if (inputadd.value !== "") {
-        addContent()
-        let spanItemAdd = document.querySelector('.item')
-        spanItemAdd.textContent = inputadd.value;
-        //
-        saveToLocal(inputadd.value);
-    }
-    // Empty The Input
-    inputadd.value = '';
-    //
-    theAddButton.disabled = true;
-}
-document.addEventListener('DOMContentLoaded', getFromLocal())
-
-//////////////////////////////////////////////// Delete Task ////////////////////////////////////////////////////
-
-
-ulList.addEventListener('click', function(event) {
-    // if element .className = delete remove
-    if (event.target.className == 'delete') {
-        if (confirm("Are you sure you want to delete")) {
-            // delete it!
-            let itemRemoved = event.target.parentNode.remove();
-            removeFromLocal(itemRemoved)
-        } else
-        // disactive type radio
-            event.target.checked = false
-    }
+var arrayTasks = [];
+let d = arrayTasks.map((item) => {
+    item = { id: Date.now() }
 })
+getFromLocal()
 
-///////////////////////////////////////////////// search  ////////////////////////////////////////////////////
-
-const search = document.querySelector(".input")
-    //get input
-let list = document.querySelector(".tasks-content")
-    //get list 
-search.addEventListener('keyup', function(e) {
-    // attach value to let
-    let inputSearching = e.target.value;
-    // get li 
-    let liOful = list.querySelectorAll('li');
-    // list of dom to array js
-    let arrayJs = Array.from(liOful)
-        // loop over each il
-    arrayJs.forEach(function(liOful) {
-        let contentItem = liOful.lastElementChild.textContent;
-        if (contentItem.includes(inputSearching)) {
-            liOful.style.display = "block"
-        } else {
-            liOful.style.display = "none"
-        }
-
-    })
-
-});
-/////////////////////////////////////////////////////////////////  local storage  //////////////////////////////////////////////////////
-function saveToLocal(value) {
-    if (JSON.parse(localStorage.getItem('Local')) == null) {
-        var boxLocal = [];
-    } else {
-        boxLocal = JSON.parse(localStorage.getItem('Local'))
-    }
-    boxLocal.push(value);
-    localStorage.setItem('Local', JSON.stringify(boxLocal));
-};
-//////////////////////////////////////////////////////
-function getFromLocal() {
-    oldBox = JSON.parse(localStorage.getItem('Local'));
-    oldBox.map(item => {
-        addContent()
-        let spanItemAdd = document.querySelector('.item')
-        spanItemAdd.textContent = item;
+// render
+function render(arrayTasks) {
+    ulListElemnt.innerHTML = ""
+    arrayTasks.map((task) => {
+        let itemAdd = document.createElement("li");
+        itemAdd.className = 'task-box';
+        ulListElemnt.prepend(itemAdd);
+        let inputRadio = document.createElement("input");
+        inputRadio.setAttribute('type', 'radio')
+        inputRadio.className = "delete";
+        itemAdd.append(inputRadio);
+        let span = document.createElement('span')
+        span.className = "item";
+        itemAdd.append(span);
+        span.textContent = task.value;
     })
 }
 
-//////////////////////////////////////////////////////
+// Add to do
+buttonSubmitElemnt.onclick = function() {
+        if (inputElemnt.value.trim() !== "") {
+            var todo = { value: inputElemnt.value, id: Date.now() }
+            arrayTasks.push(todo)
+            render(arrayTasks)
+            saveToLocal(todo)
+        }
+        inputElemnt.value = '';
+        buttonSubmitElemnt.disabled = true;
+    }
+    //search
+searchElemnt.addEventListener("keyup", () => {
+    const search = searchElemnt.value;
+    let lowerCase = search.toLowerCase()
+    const filtered = arrayTasks.filter((todo) => {
+        let itemArray = todo.value;
+        let lowerCase2 = itemArray.toLowerCase()
+        return lowerCase2.includes(lowerCase);
+    });
+
+    render(filtered)
+});
+//remove
+
+ulListElemnt.addEventListener('click', function(event) {
+        // if element .className = delete remove
+        if (event.target.className == 'delete') {
+            if (confirm("Are you sure you want to delete")) {
+                // delete it!
+                let itemRemoved = event.target.nextSibling.innerText
+                event.target.parentNode.remove();
+                removeFromLocal(itemRemoved)
+            } else
+            // disactive type radio
+                event.target.checked = false
+        }
+    })
+    //
+
+inputElemnt.addEventListener('keyup', function(e) {
+    if (e.target.value.trim() !== "") {
+        buttonSubmitElemnt.disabled = false;
+    }
+
+});
+//
+
+function saveToLocal(value) {
+    if (JSON.parse(localStorage.getItem('Local')) === null) {
+        var arrayTasks = [];
+    } else {
+        arrayTasks = JSON.parse(localStorage.getItem('Local'))
+    }
+    arrayTasks.push(value);
+    localStorage.setItem('Local', JSON.stringify(arrayTasks));
+};
+
+function getFromLocal() {
+    if (localStorage.getItem('Local') !== null) {
+        arrayTasks = JSON.parse(localStorage.getItem('Local'))
+    }
+    render(arrayTasks)
+
+};
 
 function removeFromLocal(x) {
     oldBox = JSON.parse(localStorage.getItem('Local'));
-    oldBox.splice(oldBox.indexOf(x), 1)
+    oldBox.map((todo) => {
+        return todo.value.includes(x)
+    });
+    oldBox.splice(oldBox.indexOf(x.id), 1)
     localStorage.setItem('Local', JSON.stringify(oldBox));
-
 };
-
-
-
-
-/*
-function saveToLocal(item) {
-    localStorage.setItem('LocalOfString', item);
-};
-//
-function getFromLocal() {
-    let itemA = localStorage.getItem('LocalOfString');
-    if (itemA !== null) {
-
-        addContent()
-        let spanItemAdd = document.querySelector('.item')
-        spanItemAdd.textContent = itemA;
-
-    }
-
-}
-
-function removeFromLocal() {
-
-    localStorage.removeItem('LocalOfString')
-}
-*/
